b889b373bb2213d2df40535394d3c7147ecfe771
syamlal <syamlalms8@gmail.com>
2026-01-04 12:53:59 +0400

    API_BASE_URL test.

--- sms-frontend/src/api/axios.ts
+++ sms-frontend/src/api/axios.ts
@@ -2,6 +2,7 @@
 import axios from 'axios';
 
 const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api';
+console.log("Axios baseURL:", API_BASE_URL);
 
 const api = axios.create({
   baseURL: API_BASE_URL,
